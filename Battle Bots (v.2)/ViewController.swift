581b764b7c841a9f9c0fa85a284098e505ecab01
Adam Cubas <acubas4533@stu.d214.org>
2022-01-27 11:07:29 -0600

    added "let database"

--- Battle Bots (v.2)/ViewController.swift
+++ Battle Bots (v.2)/ViewController.swift
@@ -5,15 +5,20 @@
 //  Created by Andrew Nowak on 1/25/22.
 // hog rider
 
+import Firebase
 import UIKit
 
 class ViewController: UIViewController {
 
+    let database = Database.database().reference().child("Tournament")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func getData() {
+        
+    }
 }
 
