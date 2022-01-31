da09cde9d9dda7c4c2a4a434b591c351d8c74b02
Ashley Saju <ashley.saju67@gmail.com>
2022-01-31 10:58:29 -0600

    initializers

--- Battle Bots (v.2)/Tournament.swift
+++ Battle Bots (v.2)/Tournament.swift
@@ -9,12 +9,25 @@ import Foundation
 import Firebase
 
 
-class Tournament(){
+class Tournament {
     
     var colors: [String] = []
     var teamNames: [String] = []
-    var teams: [String: String] = [teamNames: colors]
+    var teams: [[String]: [String]] = [:]
     var playerNames: [String] = []
     var winners: [String] = []
 
+    
+    init(){
+        
+        
+    }
+    init(colors: [String], teamNames: [String], teams: [:], playerNames: [String], winners: [String]){
+        teams = [teamNames: colors]
+        playerNames = playerNames.self
+        winners = winners.self
+        teamNames = teamNames.self
+        colors = colors.self
+    }
+    
 }
