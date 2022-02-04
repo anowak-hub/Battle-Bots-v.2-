b6ea6548bade87d52e882f232a623810af029a87
Adam Cubas <acubas4533@stu.d214.org>
2022-02-04 10:32:02 -0600

    Added Judge

---
+++ Battle Bots (v.2)/Judge.swift
@@ -0,0 +1,17 @@
+//
+//  Judge.swift
+//  Battle Bots (v.2)
+//
+//  Created by Adam Cubas on 2/4/22.
+//
+
+import UIKit
+
+class Judge: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
