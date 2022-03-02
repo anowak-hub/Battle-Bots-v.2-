bb30a2a134394f898b2ebe7d8e10342d7d89ec6d
Ashley Saju <ashley.saju67@gmail.com>
2022-03-02 10:37:41 -0600

    judge team labels and buttons

--- Battle Bots (v.2)/Judge.swift
+++ Battle Bots (v.2)/Judge.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class Judge: UIViewController {
 
+    @IBOutlet weak var team1Label: UIStackView!
+    @IBOutlet weak var team2Label: UIStackView!
+    @IBOutlet weak var team1Button: UIButton!
+    @IBOutlet weak var team2Button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
